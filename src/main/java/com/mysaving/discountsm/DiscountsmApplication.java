e98bf5e39bd049c13c36961408f618cad67a2a85
Jason Costa <jason.costa@starlingbank.com>
2021-06-06 11:01:36 +0100

    Initial commit

---
+++ src/main/java/com/mysaving/discountsm/DiscountsmApplication.java
@@ -0,0 +1,13 @@
+package com.mysaving.discountsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysaving/discountsm/DiscountsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysaving.discountsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
