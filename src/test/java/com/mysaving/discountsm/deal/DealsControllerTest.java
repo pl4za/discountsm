43bc7f904bd387d8707df32e3ce1be764bd810e0
Jason Costa <jason.costa@starlingbank.com>
2021-06-16 14:31:11 +0100

    Remove date time comparison

--- src/test/java/com/mysaving/discountsm/deal/DealsControllerTest.java
+++ src/test/java/com/mysaving/discountsm/deal/DealsControllerTest.java
@@ -6,8 +6,6 @@ import static org.joda.money.CurrencyUnit.GBP;
 
 import com.mysaving.discountsm.common.UUIDEntity;
 import org.joda.money.Money;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,18 +37,14 @@ class DealsControllerTest {
         DealEntity::getDescription,
         DealEntity::getNewPrice,
         DealEntity::getOldPrice,
-        DealEntity::getScore,
-        DealEntity::getPosted,
-        DealEntity::getExpiry
+        DealEntity::getScore
     ).containsOnly(
         tuple(
             "Untitled Goose Game for Nintendo Switch (Argos price match) +£3.99 non Prime",
             "Amazon have price matched Argos' price for this Nintendo Switch game. If you were after it from Argos and couldn't find stock before it sold out, this will hopefully be luckier for you! £3.99 shipping if you're not a Prime customer.",
             Money.of(GBP, 16.99),
             Money.of(GBP, 20.99),
-            0,
-            new DateTime(2014, 12, 20, 2, 30, DateTimeZone.forID("Europe/London")),
-            new DateTime(2014, 12, 20, 2, 30, DateTimeZone.forID("Europe/London")).plusDays(5)
+            0
         ));
   }
 
